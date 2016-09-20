607f1d5d43b68e985793f511f1e862e3bd7d32c9
zschur <zachschur@gmail.com>
2016-09-19 20:06:47 -0400

    passes 9 test

--- Maze.py
+++ Maze.py
@@ -4,3 +4,18 @@ import turtle
 class Maze():
     def __init__(self):
         self.s = turtle.Screen()
+        self.t = turtle.Turtle()
+        self.s.bgcolor('blue')
+        self.s.register_shape("custom",((10, 10),(10, -10),\
+                                        (10,-10),(-10,-10),\
+                                        (-10,-10),(-10,10),\
+                                        (-10,10), (10,10)))
+        self.t.shape("custom")
+        self.matrix = [[1 for i in range(20)] for i in range(20)]
+        self.t.color('white')
+        self.t.penup()
+        self.t.goto(-190,190)
+        self.matrix[0][0]=0
+        self.s.screensize(400,400)
+        
+        
--- MazeTests.py
+++ MazeTests.py
@@ -1,27 +1,61 @@
-from Maze import *
 import random
 import turtle
 import unittest
+from Maze import *
 
 class testMaze(unittest.TestCase):
+    """
+    This class inherits from a class called TestCase which is
+    defined in the unittest module.
+
+    When you inherit from a class, you get all the methods that are
+    defined in that class for free.
+
+    Since this is a TestCase class, calling unittest.main() will automatically
+    run any of the functions that start with the word 'test'
+    """
+    
     def setUp(self):
-        
-        # this checks for a Maze class
+        """
+        The setUp function is called each time one of your tests is run.
+        We create an instance of the maze here before each test.
+        """
         self.m=Maze()
 
     def testMazeExists(self):
-
+        """
+        this will check to see if we have a maze class but as soon
+        as setUp is run, we will see a failure so we really don't need
+        to do anything here
+        """
         pass
-
+    
     def testScreenExists(self):
-        assert type(self.m) == turtle._Screen
+        assert type(self.m.s) == turtle._Screen
 
     def testTurtleExists(self):
-        assert type(self.m.t) == turtle._Turtle
+        assert type(self.m.t) == turtle.Turtle
 
+    def testScreenBackgroundIsBlue(self):
+        assert self.m.s.bgcolor()=='blue'
 
+    def testForMatrix(self):
+        assert len(self.m.matrix)==20
+
+    def testTurtleIsWhite(self):
+        assert self.m.t.color()[0]=='white' and self.m.t.color()[1]=='white'
+
+    def testTurtleIsInUpperLeftHandCorner(self):
+        assert self.m.t.pos()==(-190,190), "Turtle position is %d,%d" % \
+               (self.m.t.pos()[0],self.m.t.pos()[1])
+        
+    def testTurtleMatrixIs0InLeftHandCorner(self):
+        assert self.m.matrix[0][0]==0
+
+    def testScreenSize(self):
+        assert self.m.s.screensize()==(400,400)
+        
+        
+        
 if __name__=="__main__":
-    unittest.main(exit=False)
-
-unittest.main()
-
+    unittest.main()
